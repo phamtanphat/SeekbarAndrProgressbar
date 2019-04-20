a1109f6d9ebd75cbab126729dfad90a8fe501e73
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-20 21:35:17 +0700

    A

--- app/src/main/java/khoapham/ptp/phamtanphat/seekbarandprogressbar/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/seekbarandprogressbar/MainActivity.java
@@ -1,13 +1,49 @@
 package khoapham.ptp.phamtanphat.seekbarandprogressbar;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        seekBar = findViewById(R.id.seekbar);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                Log.d("BBB",progress + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        progressBar.setProgress(50);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                seekBar.setProgress(seekBar.getProgress() + 10);
+                handler.postDelayed(this,1000);
+            }
+        },1000);
     }
 }
